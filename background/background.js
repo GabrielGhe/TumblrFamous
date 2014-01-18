27ebdee0eafc14c7eb8ddd13c0a86386e85805a5
Matt <matthew.kuzyk@gmail.com>
2014-01-18 10:28:29 -0500

    Changed the headers, added some oauth stuff

--- background/background.js
+++ background/background.js
@@ -1,3 +1,30 @@
+var tumblr = require('tumblr.js');
+
+var oauth = ChromeExOAuth.initBackgroundPage({
+  'request_url': "http://www.tumblr.com/oauth/request_token",
+  'authorize_url': "http://www.tumblr.com/oauth/authorize",
+  'access_url': "http://www.tumblr.com/oauth/access_token",
+  'consumer_key': "NugbEX3qh96IleVqn2eGLTphq8WWFb1NDhC15XfcdHm5xtjdVb",
+  'consumer_secret': "8j0CkWdhKKrxuokj2OZEPH43uvgFf9eReKzFYI57ubXkapBnEe"
+});
+
+function callback(resp, xhr) {
+  // ... Process text response ...
+};
+
+function onAuthorized() {
+  var url = 'https://docs.google.com/feeds/default/private/full';
+  var request = {
+    'method': 'GET',
+    'parameters': {'alt': 'json'}
+  };
+
+  // Send: GET https://docs.google.com/feeds/default/private/full?alt=json
+  oauth.sendSignedRequest(url, callback, request);
+};
+
+oauth.authorize(onAuthorized);
+
 var bg = {  
   saveTags: function(post) {
       // by passing an object you can define default values e.g.: []
